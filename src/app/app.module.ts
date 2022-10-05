7fc2a2ff8c5336c2743ce8d02321beed8621e36d
polgerg <polonkai.gergely92@gmail.com>
2022-10-05 13:08:31 +0200

    black-box-component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BlackBoxComponent } from './components/black-box/black-box.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BlackBoxComponent
   ],
   imports: [
     BrowserModule,
